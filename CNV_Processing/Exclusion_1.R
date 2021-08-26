d5d774cfa9411a4e03fd86eaa3a60af92c3d3061
Huffnagle <huffnaglen@f8ffc2583d87.myfiosgateway.com>
2021-08-26 12:30:17 -0400

    Tidied script
    Removed manual counts of remaining CNV at each exclusion step. Also added a header at the top of script to match Git naming convention and description.

--- CNV_Processing/Exclusion_1.R
+++ CNV_Processing/Exclusion_1.R
@@ -1,9 +1,12 @@
+# Exclusion_1 script
+# Takes data from CNVision merge format and applies chip quality control and markers criteria (detailed in SFigs. 1-2).
+
 # Load packages
 library(tidyverse)
 
 # Load data
 path <- "/Users/huffnaglen/PNC CNV Project copy/Analysis/RData"
-load(paste(path,'chip.qc.cag.RData',sep = '/')) # this is illumina chip dataset with all chip level qc
+load(paste(path,'chip.qc.cag.RData',sep = '/'))
 load(paste(path,'full.cnvision_merge.RData',sep = '/'))
 chip.markers <- read.csv(paste(path,'chipmarkers.csv',sep = '/'))
 
@@ -13,68 +16,49 @@ names(ncnvs)[1] <- 'chipids'
 
 newmerge <- merge(chip.markers,ncnvs,by = 'chipids')
 
+# Markers - CNV criteria
 cleanmerge <- newmerge %>%
   filter(Freq<50 | nmarkers > 1000000) %>%
   filter(Freq<200)
 
 cleanmerge$ChipID <- unlist(strsplit(cleanmerge$chipids,'.int.csv'))
 
-# 1. count raw
-length(unique(chip.qc.cag$ChipID)) # 10622
-length(unique(chip.qc.cag$cag_id)) # 8067
-
 # Remove GH/Seb chips
 chip.qc.cag <- chip.qc.cag %>% 
   filter(ChipID %in% cleanmerge$ChipID)
 
-length(unique(chip.qc.cag$ChipID)) # 9483
-length(unique(chip.qc.cag$cag_id)) # 7697
-
-# 2. exclude baf, lrr, wf
+# Exclude baf, lrr, wf
 chip.qc.cag <- chip.qc.cag %>% 
   filter(lrrsd<0.35)
-length(unique(chip.qc.cag$ChipID)) # 9458
-length(unique(chip.qc.cag$cag_id)) # 7679
 
 chip.qc.cag <- chip.qc.cag %>% 
   filter(bafsd<0.08)
-length(unique(chip.qc.cag$ChipID)) # 9458
-length(unique(chip.qc.cag$cag_id)) # 7679
 
 chip.qc.cag <- chip.qc.cag %>% 
   filter(abs(wavef)<0.05)
-length(unique(chip.qc.cag$ChipID)) # 9388
-length(unique(chip.qc.cag$cag_id)) # 7671
 
-list.a <- chip.qc.cag %>% 
+list.a <- chip.qc.cag %>% # Save a simplified cnv quality dataset for later use.
   select(1,4:7)
-
 save(list.a,file = paste(path,'list.a2.RData',sep = '/'))
 
-# 5. Look at cnv level w/ these chips
+# Look at cnv level w/ these good quality chips
 cnv.set <- dtcnv %>% 
   filter(ChipID %in% chip.qc.cag$ChipID)
 
-# 6. Algorithm cutoffs
+# Algorithm cutoffs
 cnv.set$two.algs <- as.numeric(unlist(strsplit(cnv.set$X.Two.Algs,"%")))
 
 # Keep cnvs w/ confidence score >= 30
 cnv.set <- cnv.set %>% 
   filter(Max.Score>=30)
-length(unique(cnv.set$ChipID)) # 8997
-length(unique(cnv.set$cag_id)) # 7610
 
 # Keep cnvs w/ both algorithm
 cnv.set <- cnv.set %>%
   filter(X.Algos==2)
-length(unique(cnv.set$ChipID)) # 8880
-length(unique(cnv.set$cag_id)) # 7578
 
 # Keep cnvs w/ 70% algorithm overlap
 cnv.set <- cnv.set %>% 
   filter(two.algs>=70)
-length(unique(cnv.set$ChipID)) # 8761
-length(unique(cnv.set$cag_id)) # 7541
 
 list.b <- chip.qc.cag %>% 
   filter(ChipID %in% cnv.set$ChipID)
