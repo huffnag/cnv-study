6d4b35e0a1d45339f29f703657446b4d32dfef7f
Aaron Alexander-Bloch <aalexanderbloch@gmail.com>
2021-09-20 13:58:41 -0400

    Update Figure_4.R

--- Results/Figure_4.R
+++ Results/Figure_4.R
@@ -1,9 +1,10 @@
-#analyze quanttraits
+#Structural MRI normative model results
 library(tidyverse)
 
-path <- "/Users/huffnaglen/"
+path <- '/Users/aa2227/Documents/pncCNV/clean'
+figpath <- paste(path,'figs',sep='/')
 
-brainlof <- read.csv(paste(path,'brainlof.csv',sep = '/'))
+brainlof <- read.csv(paste(path,'brainlof_qc.csv',sep = '/'))
 
 brainlof <- brainlof[,-(2:3)]
 
@@ -22,9 +23,9 @@ infranormal.all <- apply(infranormal,1,any)
 
 deviat.all <- infranormal.all | supranormal.all
 
-lof.cat <- rep('low_risk', length(brainlof$pHI))
+lof.cat <- rep('low risk', length(brainlof$pHI))
 #lof.cat[brainlof$pHI>0 | brainlof$pTS>0 ] <- '0-1'
-lof.cat[brainlof$pHI>(1) | brainlof$pTS>(1)] <- 'high_risk'
+lof.cat[brainlof$pHI>(1) | brainlof$pTS>(1)] <- 'high risk'
 
 toplot.raw <- table(lof.cat,deviat.all)
 chisq.test(toplot.raw)
@@ -34,20 +35,19 @@ toplot[1,] <- toplot[1,]/sum(toplot[1,])
 toplot[2,] <- toplot[2,]/sum(toplot[2,])
 
 toplotdf <- data.frame(proportion=c(toplot[,1],toplot[,2]), CNV_Intolerance_Score=c(rownames(toplot), rownames(toplot)), 
-  growthcart=c('low_deviance', 'low_deviance','high_deviance','high_deviance'))
-
+  growthcart=c('low deviance', 'low deviance','high deviance','high deviance'))
 p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=proportion, fill=growthcart)) +
 geom_bar(stat="identity", color="black", position=position_dodge())+
   theme_minimal()
 
-lab <- c('','n=919','n=60','')
+lab <- c('','n=891','n=59','')
 toplotdf$lab <- lab
 # Use custom colors
-p + scale_fill_manual(values=c('#999999','#E69F00'))
+p + scale_fill_manual(values=c("red","#3B9AB2"))
 
 p+
   geom_text(data=toplotdf,aes(x=CNV_Intolerance_Score,y=proportion,label=lab),vjust=-.4,hjust=1)+
-  scale_fill_manual(values=c('#999999','#E69F00'))+ 
+  scale_fill_manual(values=c("red","#3B9AB2"))+ 
   ggtitle("CNV risk and brain deviance")  + 
   theme(plot.title = element_text(hjust = 0.5))+
   ylab('Proportion')+
@@ -55,49 +55,39 @@ p+
   theme(text=element_text(size=16))
 
 
-ggsave('~/Documents/pncCNV/figures/braindev_main.pdf')
+ggsave(paste(figpath,'fig4.pdf',sep = '/'))
 
+###Supplemental plots by phenotype
 
-
-
-
-###supp plots by phenotype
-
-lof.cat <- rep('low_risk', length(brainlof$pHI))
+lof.cat <- rep('low risk', length(brainlof$pHI))
 #lof.cat[brainlof$pHI>0 | brainlof$pTS>0 ] <- '0-1'
-lof.cat[brainlof$pHI>(1) | brainlof$pTS>(1)] <- 'high_risk'
+lof.cat[brainlof$pHI>(1) | brainlof$pTS>(1)] <- 'high risk'
 
 toplotdf <- data.frame(CNV_Intolerance_Score=lof.cat, GMV_quantile=brainlof$GMV,  WMV_quantile=brainlof$WMV, sGMV_quantile=brainlof$sGMV)
 
 
 p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=GMV_quantile)) +
-geom_violin()+geom_jitter() + theme_minimal()
+geom_violin()+geom_jitter() + theme_minimal() +  xlab("CNV Intolerance Score") + ylab("GMV centile")
   p
-  ggsave('~/Documents/pncCNV/figures/braindev_suppGMV.pdf')
+ggsave(paste(figpath,'suppGMV.pdf',sep = '/'))
 
   p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=WMV_quantile)) +
-geom_violin()+geom_jitter() + theme_minimal()
+geom_violin()+geom_jitter() + theme_minimal() +  xlab("CNV Intolerance Score") + ylab("WMV centile")
   p
-  ggsave('~/Documents/pncCNV/figures/braindev_suppWMV.pdf')
+ggsave(paste(figpath,'suppWMV.pdf',sep = '/'))
 
 
 p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=sGMV_quantile)) +
-geom_violin()+geom_jitter() + theme_minimal()
+geom_violin()+geom_jitter() + theme_minimal() +  xlab("CNV Intolerance Score") + ylab("sGMV centile")
   p
-  ggsave('~/Documents/pncCNV/figures/braindev_suppSGMV.pdf')
+ggsave(paste(figpath,'suppSubGMV.pdf',sep = '/'))
+
+##redo with LOEUF
 
 
-
-
-# Use brewer color palettes
-
-
-##redo with LOF
-
-
-lof.cat <- rep('low_risk', length(brainlof$pHI))
+lof.cat <- rep('low risk', length(brainlof$pHI))
 #lof.cat[brainlof$pHI>0 | brainlof$pTS>0 ] <- '0-1'
-lof.cat[brainlof$LOF>(1/.34)] <- 'high_risk'
+lof.cat[brainlof$LOF>(1/.34)] <- 'high risk'
 
 #lof.cat <- rep('0', length(brainlof$LOF))
 #lof.cat[brainlof$LOF>0] <- '0-3'
@@ -111,29 +101,29 @@ toplot[1,] <- toplot[1,]/sum(toplot[1,])
 toplot[2,] <- toplot[2,]/sum(toplot[2,])
 
 toplotdf <- data.frame(proportion=c(toplot[,1],toplot[,2]), CNV_Intolerance_Score=c(rownames(toplot), rownames(toplot)), 
-  growthcart=c('low_deviance', 'low_deviance','high_deviance','high_deviance'))
+  growthcart=c('low deviance', 'low deviance','high deviance','high deviance'))
 
 p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=proportion, fill=growthcart)) +
 geom_bar(stat="identity", color="black", position=position_dodge())+
   theme_minimal()
 
-lab <- c('','n=941','n=38','')
+lab <- c('','n=912','n=38','')
 toplotdf$lab <- lab
 # Use custom colors
-p + scale_fill_manual(values=c('#999999','#E69F00'))
+p + scale_fill_manual(values=c("red","#3B9AB2"))
 
-p+geom_text(data=toplotdf,aes(x=CNV_Intolerance_Score,y=proportion,label=lab),vjust=-1)+ scale_fill_manual(values=c('#999999','#E69F00')) +
-ggtitle("CNV risk and brain deviance \n (LOEUF annotation)")  + theme(plot.title = element_text(hjust = 0.5))
-ggsave('~/Documents/pncCNV/figures/braindev_supp1.pdf')
+p+geom_text(data=toplotdf,aes(x=CNV_Intolerance_Score,y=proportion,label=lab),vjust=-1)+ scale_fill_manual(values=c("red","#3B9AB2")) +
+ggtitle("CNV risk and brain deviance \n (LOEUF annotation)")  + theme(plot.title = element_text(hjust = 0.5)) +  xlab("CNV Intolerance Score")
 
+ggsave(paste(figpath,'supp_braindev_LOEUF.pdf',sep = '/'))
 
 
 ##redo with LOF cont
 
-lof.cat <- rep('C_low_risk', length(brainlof$pHI))
+lof.cat <- rep('C) low risk', length(brainlof$pHI))
 #lof.cat[brainlof$pHI>0 | brainlof$pTS>0 ] <- '0-1'
-lof.cat[brainlof$LOF>0] <- 'B_medium_risk'
-lof.cat[brainlof$LOF>(1/.34)] <- 'A_high_risk'
+lof.cat[brainlof$LOF>0] <- 'B) medium risk'
+lof.cat[brainlof$LOF>(1/.34)] <- 'A) high risk'
 
 toplot.raw <- table(lof.cat,deviat.all)
 chisq.test(toplot.raw)
@@ -143,8 +133,8 @@ toplot[2,] <- toplot[2,]/sum(toplot[2,])
 toplot[3,] <- toplot[3,]/sum(toplot[3,])
 
 toplotdf <- data.frame(proportion=c(toplot[,1],toplot[,2]), CNV_Intolerance_Score=c(rownames(toplot), 
-  rownames(toplot)), growthcart=c('low_deviance', 'low_deviance','low_deviance', 
-  'high_deviance','high_deviance', 'high_deviance'))
+  rownames(toplot)), growthcart=c('low deviance', 'low deviance','low deviance', 
+  'high deviance','high deviance', 'high deviance'))
 
 
 
@@ -152,11 +142,11 @@ p <- ggplot(data=toplotdf, aes(x=CNV_Intolerance_Score, y=proportion, fill=growt
 geom_bar(stat="identity", color="black", position=position_dodge())+
   theme_minimal()
 
-lab <- c('','n=217','n=724','n=38','', '')
+lab <- c('','n=210','n=702','n=38','', '')
 toplotdf$lab <- lab
 # Use custom colors
 
-p+geom_text(data=toplotdf,aes(x=CNV_Intolerance_Score,y=proportion,label=lab),vjust=-1)+ scale_fill_manual(values=c('#999999','#E69F00')) +
-ggtitle("CNV risk and brain deviance \n (LOEUF annotation with medium risk)")  + theme(plot.title = element_text(hjust = 0.5))
-ggsave('~/Documents/pncCNV/figures/braindev_supp2.pdf')
+p+geom_text(data=toplotdf,aes(x=CNV_Intolerance_Score,y=proportion,label=lab),vjust=-1)+ scale_fill_manual(values=c("red","#3B9AB2")) +
+ggtitle("CNV risk and brain deviance \n (LOEUF annotation with medium risk)")  + theme(plot.title = element_text(hjust = 0.5)) +  xlab("CNV Intolerance Score")
+ggsave(paste(figpath,'supp_braindev_LOEUF_v2.pdf',sep = '/'))
 
